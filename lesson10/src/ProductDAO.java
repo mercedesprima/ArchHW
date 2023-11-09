53f497605e7bd79193cfefc6c2a34adfaa462639
Ekaterina_P <zazie25@yandex.ru>
2023-11-09 19:16:19 +0300

    lesson10 added

---
+++ lesson10/src/ProductDAO.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDAO {
+    List<Product> productDao = new ArrayList<>();
+    public void add(Product product) {
+    }
+
+    public void delete(int id) {
+    }
+
+    public List<Product> getAll() {
+        return productDao;
+    }
+    // Методы для добавления, удаления и получения продуктов из базы данных (или списка)
+}
